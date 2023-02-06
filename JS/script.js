0e2da1159351eb3db99a8d996dd35e12b18c023e
GustavoOmai <omai.gustavoh@gmail.com>
2023-02-06 04:42:05 -0300

    Section Tecnology

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,29 @@ function hamburguer(){
         menu.classList.add('display-none')
     }
 }
+$(document).ready(function () {
+$('.tecnologias-slick').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 5,
+    speed: 300,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+    ]
+  });
+});
