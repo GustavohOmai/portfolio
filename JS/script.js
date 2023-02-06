c553f9bd6eda93f6ffa735374744024027632c36
GustavoOmai <omai.gustavoh@gmail.com>
2023-02-06 05:46:33 -0300

    Version 1.0

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,22 @@
 let checkbox = document.getElementById('checkbox-menu')
 let menu = document.getElementById('nav-mob')
 
-function hamburguer(){
+function hamburguer() {
+  console.log("Foi caralho");
+  if (checkbox.checked) {
+    menu.classList.remove('display-none')
     console.log("Foi caralho");
-    if(checkbox.checked){
-        menu.classList.remove('display-none')
-        console.log("Foi caralho");
-    }
-    else{
-        menu.classList.add('display-none')
-    }
+  } else {
+    menu.classList.add('display-none')
+  }
 }
 $(document).ready(function () {
-$('.tecnologias-slick').slick({
+  $('.tecnologias-slick').slick({
     dots: false,
     infinite: true,
     slidesToShow: 5,
     speed: 300,
-    responsive: [
-      {
+    responsive: [{
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
@@ -36,4 +34,40 @@ $('.tecnologias-slick').slick({
       },
     ]
   });
+
+
+  $('.projetos-destaque').slick({
+    mobileFirst: true,
+    responsive: [{
+      breakpoint: 480,
+      settings: "unslick"
+    }]
+  });
+
+
+
+  $('.projetos-todos').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    speed: 300,
+    responsive: [{
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
+  });
+
 });
